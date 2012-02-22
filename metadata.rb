6b666ca4ac60f6784a8188f43578857b64194cc8
Noah Kantrowitz <noah@coderanger.net>
2012-02-21 18:42:55 -0800

    Initial commit of collectd cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Noan Kantrowitz"
+maintainer_email "noah@coderanger.net"
+license          "Apache 2.0"
+description      "Install and configure the collectd monitoring daemon"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+supports         "ubuntu"
