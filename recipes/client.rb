3a24a852715467347c79dd6383196c9e8e35b186
Holger Amann <holger@sauspiel.de>
2013-10-08 11:54:55 +0200

    search for nodes in same chef environment

--- recipes/client.rb
+++ recipes/client.rb
@@ -20,7 +20,7 @@
 include_recipe "collectd"
 
 servers = []
-search(:node, 'recipes:collectd\\:\\:server') do |n|
+search(:node, "recipes:collectd\\:\\:server AND chef_environment:#{node.chef_environment}") do |n|
   servers << n['fqdn']
 end
 
